6d9efe75dbbd0fda34386ab0e79ca34d30feea3d
Omar Hakem <youtarawneh997@gmail.com>
2021-04-11 23:58:09 +0300

    queue with two stacks

---
+++ javascript/queueWithStacks/queue-with-stacks.js
@@ -0,0 +1,29 @@
+"use strict";
+const { Stack } = require("../stacksAndQueues/stacks-and-queues.js");
+class PseudoQueue {
+  constructor() {
+    this.pushStack = new Stack();
+    this.popStack = new Stack();
+  }
+  enqueue(value) {
+    this.pushStack.push(value);
+  }
+  dequeue() {
+    // if the pop stack is empty push the nodes from the pushStack to popStack and pop the first
+    if (this.popStack.isEmpty() && !this.pushStack.isEmpty()) {
+      while (this.pushStack.top) {
+        this.popStack.push(this.pushStack.top.value);
+        this.pushStack.top = this.pushStack.top.next;
+      }
+      this.popStack.pop();
+      // if both stacks  empty Exception
+    } else if (this.pushStack.isEmpty() && this.popStack.isEmpty()) {
+      return "Exception: Stacks are empty";
+      // popStack is not empty so pop the first node
+    } else {
+      this.popStack.pop();
+    }
+  }
+}
+
+module.exports = PseudoQueue;
