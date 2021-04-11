6ecc414ec231668ad411402df27373f3897a1719
Omar Hakem <youtarawneh997@gmail.com>
2021-04-11 21:17:19 +0300

    solving the challenge 11 stack and queue

--- javascript/stacksAndQueues/stacks-and-queues.js
+++ javascript/stacksAndQueues/stacks-and-queues.js
@@ -86,12 +86,6 @@ class Queue {
   }
 }
 
-let queue = new Queue();
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-queue.dequeue();
-console.log(queue);
 module.exports = {
   Stack,
   Queue,
